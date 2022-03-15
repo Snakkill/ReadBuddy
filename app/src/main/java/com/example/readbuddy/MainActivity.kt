18f6dd2b37b78e4e0339f0ab0a8329157b779cd0
Tyler Kauffman <tak4506@gmail.com>
2022-03-15 15:39:25 -0500

    Created 3 HomeFragment.kt, LeaderboardFragment.kt and ReadingListFragment.kt, bound to bottom navigation and working switch.

--- app/src/main/java/com/example/readbuddy/MainActivity.kt
+++ app/src/main/java/com/example/readbuddy/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.readbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.readbuddy.R
+import com.example.readbuddy.fragments.HomeFragment
+import com.example.readbuddy.fragments.LeaderboardFragment
+import com.example.readbuddy.fragments.ReadingListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val leaderboardFragment = LeaderboardFragment()
+    private val readingListFragment = ReadingListFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Set up the bottom navigation
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        replaceFragment(homeFragment)
+        // Deprecated, look into a different way to do this
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> replaceFragment(homeFragment)
+                R.id.menu_leaderboard -> replaceFragment(leaderboardFragment)
+                R.id.menu_reading_list -> replaceFragment(readingListFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 
     override fun onBackPressed() {
