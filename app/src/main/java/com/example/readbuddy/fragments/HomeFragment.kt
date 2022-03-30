9ec2b426b89c7ac1eecc53ed38a782981e9e84bc
Tyler Kauffman <tyler.kauffman@cognizant.com>
2022-03-30 08:17:41 -0500

    Carousel populated with images.

--- app/src/main/java/com/example/readbuddy/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/readbuddy/fragments/HomeFragment.kt
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.SearchView
+import android.widget.*
+import androidx.cardview.widget.CardView
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.example.readbuddy.BookInfo
 import com.example.readbuddy.R
 import com.example.readbuddy.SearchResults
+import kotlinx.android.synthetic.main.activity_maps.*
+import org.w3c.dom.Text
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -34,9 +37,9 @@ class HomeFragment : Fragment() {
     ): View? {
         // Create the screen to access view elements in the layout.
         val screen = inflater.inflate(R.layout.fragment_home, container, false)
+
         val button = screen.findViewById<Button>(R.id.idBtnSearch)
-        //val carousel = screen.findViewById<LinearLayout>(R.id.book_carousel)
-        // Handle the search function
+
         button.setOnClickListener{
             val searchText = screen.findViewById<EditText>(R.id.searchView)
             val intent = Intent(activity, SearchResults::class.java).apply {
@@ -46,10 +49,26 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
 
+        val carouselLayout = screen.findViewById<LinearLayout>(R.id.linearLayout_carousel)
+        val carouselInflater = LayoutInflater.from(activity)
+
+        val bookList = getDummyBooks()
+
+        // populate the carousel
+
+        for (book in bookList) {
+            val view = carouselInflater.inflate(R.layout.carousel_item, carouselLayout, false)
+            val thumbnail = view.findViewById<ImageView>(R.id.iv_carouselThumb)
+            //val title = view.findViewById<TextView>(R.id.tv_bookTitle)
+            //title.text = book.title
+            Glide.with(view)
+                .load(book.thumb)
+                .into(thumbnail)
+            carouselLayout.addView(view)
+        }
+
         return screen
     }
-
-
 
     companion object {
         @JvmStatic
@@ -61,4 +80,25 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+    private fun getDummyBooks(): ArrayList<Book> {
+        // Generate 5 books with static content for demonstration purposes.  NOT FINAL.  SERIOUSLY, DON'T EVER DO THIS
+        val bookList = arrayListOf<Book>()
+        val dummyBooks = mapOf(
+            "Think Java" to "https://books.google.com/books/content?id=XYQpDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            "Java Generics and Collections" to "https://books.google.com/books/content?id=VUSbAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            "Effective Java" to "https://books.google.com/books/content?id=ka2VUBqHiWkC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            "Java for Students" to "https://books.google.com/books/content?id=TRUdyfwdaSoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            "TCP/IP Sockets in Java" to  "https://books.google.com/books/content?id=lfHo7uMk7r4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        )
+
+        dummyBooks.forEach { entry ->
+            val book = Book(entry.key, entry.value)
+            bookList.add(book)
+        }
+
+        return bookList
+    }
 }
+// Placeholder class, ideally would be the API provided class, but search is using volley.
+private class Book (val title : String, val thumb : String)
